9205537f3634a185b318f758129e7b0cae0f2f15
Betiza Penques <be.penques@hotmail.com>
2019-07-08 20:21:30 -0300

    Eventos de teclado

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -12,6 +12,7 @@ class CalcController{
         this.currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyBoard();
     }
 
     initialize(){
@@ -31,6 +32,51 @@ class CalcController{
         this._timeEL.innerHTML = "18:00"; */
 
         this.setLastNumberToDisplay();
+    }
+
+    //metodo para inicializar eventos de teclado na calculadora
+    initKeyBoard(){
+        document.addEventListener('keyup', e=>{
+
+            console.log(e.key);
+
+            switch(e.key){
+
+                case 'Escape':
+                    this.clearAll();
+                    break;
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+                case '+':
+                case '-':
+                case '/':
+                case '*':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+            }
+        });
     }
 
     addEventListenerAll(element, events, fn){//função para rodar cada evento por vez - click e drag
